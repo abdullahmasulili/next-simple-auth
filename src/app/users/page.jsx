4ad645df08b8ab48460752050ba11a6f403ff9bc
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-24 06:27:09 +0800

    feat(page): display users

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,21 +1,63 @@
 "use client";
 
 import Button from "@/components/Button";
+import Card from "@/components/Card";
 import { useAuth } from "@/context/AuthContext";
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 export default function Users() {
-  const { user, logout } = useAuth();
+  const { logout } = useAuth();
+  const [pageData, setPageData] = useState(null);
+
+  async function fetchUsers() {
+    try {
+      const response = await fetch("https://reqres.in/api/users?per_page=100");
+
+      if (!response.ok) {
+        throw new Error({ message: "Failed fetch users", status: 500 });
+      }
+
+      const resData = await response.json();
+      console.log(resData);
+      setPageData(resData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (!pageData) {
+      fetchUsers();
+    }
+  }, [pageData]);
 
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quo
-        numquam reiciendis, voluptatum debitis minus rerum impedit nostrum quasi
-        eligendi sint dicta culpa molestiae ea. Soluta voluptatibus eos
-        architecto velit.
-      </p>
-      <Button onClick={() => logout()}>Logout</Button>
-    </div>
+    <>
+      <header className="sticky top-0 h-fit-content bg-stone-900 flex items-center justify-between p-4">
+        <h1 className="text-xl">Welcome</h1>
+        <Button onClick={() => logout()}>Logout</Button>
+      </header>
+      <main className="flex justify-center">
+        <div className="container py-8 grid grid-cols-3 gap-2">
+          {pageData?.data.map((user) => (
+            <Card
+              key={user.last_name}
+              className="flex items-center gap-4 w-full"
+            >
+              <div className="rounded-full h-14 w-14 overflow-hidden">
+                <img src={user.avatar} alt="user-avatar" />
+              </div>
+              <div className="flex flex-col items-start gap-1">
+                <h1 className="text-xl font-bold">
+                  {user.first_name} {user.last_name}
+                </h1>
+                <p className="text-base font-thin">{user.email}</p>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
