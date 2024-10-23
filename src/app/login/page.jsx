1792e348450457ad5a70c604cd385e79c6a9070d
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-23 18:54:53 +0800

    feat(login): add login handler

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
 import AuthContainer from "@/layouts/AuthContainer";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import { useRouter } from "next/navigation";
+import { login } from "@/lib/firebaseAuth";
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    const formData = new FormData(event.target);
+    const creds = Object.fromEntries(formData.entries());
+
+    try {
+      const user = await login(creds);
+      console.log(user);
+      // router.push("/");
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <AuthContainer>
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit}>
         <Input id="email" name="email" label="Email" type="email" />
         <Input id="password" name="password" label="Password" type="password" />
-        <Button block>Sign In</Button>
-        <Button block className="bg-blue-500 mt-3">
+        <Button block type="submit">
+          Sign In
+        </Button>
+        <Button block type="button" className="bg-blue-500 mt-3">
           Sign In With Google
         </Button>
       </form>
