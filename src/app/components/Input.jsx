7941d5d10cb551cc80bbce9214aa4fb9499b11c6
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-23 13:50:45 +0800

    patch(Input): manage styling inside variable

--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 export default function Input({ id, label, ...props }) {
+  const styling = [
+    "w-full",
+    "my-2",
+    "h-10",
+    "px-3",
+    "rounded",
+    "focus:outline-none",
+    "focus:border-2",
+    "border-2",
+    "border-white",
+    "focus:border-amber-300",
+    "bg-transparent",
+  ].join(" ");
+
   return (
     <>
       <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        {...props}
-        className="w-full my-2 h-10 px-3 rounded focus:outline-none focus:border-2 border-2 border-white focus:border-amber-300 bg-transparent"
-      />
+      <input id={id} {...props} className={styling} />
     </>
   );
 }
